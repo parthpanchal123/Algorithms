020978b6636b25b7f9f89cca365ab2a956e6c5f4
parthpanchal123 <20091596+parthpanchal123@users.noreply.github.com>
2020-01-06 19:05:40 +0530

    Initial commit

---
+++ insertionsort.py
@@ -0,0 +1,19 @@
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        hole = i
+        value = arr[i]
+        while(hole > 0 and arr[hole - 1] > value):
+            arr[hole] = arr[hole-1]
+            hole -= 1
+        arr[hole] = value
+    return arr
+
+
+if __name__ == "__main__":
+    print('Enter elements to sort')
+    my_arr = list(map(int, input().split(" ")))
+    print(insertion_sort(my_arr))
+
+
+# 4 3 2 1

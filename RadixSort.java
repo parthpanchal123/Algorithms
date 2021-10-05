6ab11ca371d04c1a63836adb41cac10a17632f90
shahsemil <50923482+shahsemil@users.noreply.github.com>
2021-10-05 21:03:20 +0530

    Update RadixSort.java

--- RadixSort.java
+++ RadixSort.java
@@ -7,7 +7,7 @@ class RadixSort {
     static int getMax(int arr[], int n)
     {
         int mx = arr[0];
-        for (int i = 1; i < n; i++)
+        for (int i = 1;i < n; i++)
             if (arr[i] > mx)
                 mx = arr[i];
         return mx;
